720705f0592f0ab3fda516b91e246b3cfb456ee6
Andrii <andrii.yakovyna@gmail.com>
2020-04-29 20:38:22 +0200

    Added chat  page, middleware has been configured to forbid access to the chat page without authorization

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,7 @@
 <template>
   <v-layout column justify-center align-center>
     <v-flex xs12 sm8>
-      <v-card min-width="400" shaped="true">
+      <v-card min-width="400">
         <v-card-title>Chat Project</v-card-title>
         <v-card-text>
           <v-form ref="form" v-model="valid" lazy-validation>
@@ -24,8 +24,12 @@
 </template>
 
 <script>
+import { mapMutations } from "vuex"; //we received a mutation and now we can use it like normally method
 export default {
   layout: "empty",
+  head: {
+    title: "Welcome to my chatroom"
+  },
   sockets: {
     connect: function() {
       console.log("socket connected");
@@ -42,8 +46,17 @@ export default {
     roomRules: [v => !!v || "Write a room code"]
   }),
   methods: {
+    ...mapMutations(["setUser"]), //call mutation from store
     submit() {
-      this.$refs.form.validate();
+      if (this.$refs.form.validate()) {
+        const user = {
+          //creating user
+          name: this.name,
+          room: this.room
+        };
+        this.setUser(user); // transfer user to sever
+        this.$router.push("/chat"); //Redirect to chat page
+      }
     }
   }
 };
